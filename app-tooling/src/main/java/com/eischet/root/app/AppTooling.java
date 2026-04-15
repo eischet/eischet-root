9dfd8eef05b4d2e781137cbd0527ff1c86fdc24b
Stefan Eischet <stefan@eischet.net>
2026-04-15 14:17:17 +0200

    javadocs

--- app-tooling/src/main/java/com/eischet/root/app/AppTooling.java
+++ app-tooling/src/main/java/com/eischet/root/app/AppTooling.java
@@ -1,5 +1,10 @@
 package com.eischet.root.app;
 
+/**
+ * Helper to detect if we're running a beta version.
+ *
+ * Beta versions include the app-beta artifact / jar, which includes the BetaVersion class, and we simply try to load it.
+ */
 @SuppressWarnings("unused")
 public class AppTooling {
 
